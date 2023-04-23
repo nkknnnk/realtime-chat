49278f15756456fb670baefbfd95d3e7df9329c6
nkknnnk <nk6593289@gmail.com>
2023-04-23 08:23:44 +0530

    initial commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,6 +4,7 @@ import Button from '@/components/ui/Button'
 import { FC, useState } from 'react'
 import { signIn } from "next-auth/react"
 import { toast } from 'react-hot-toast'
+import Image from 'next/image'
 
 interface pageProps { }
 
@@ -25,7 +26,7 @@ const Page: FC<pageProps> = ({ }) => {
         <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
             <div className="w-full flex flex-col items-center max-w-md space-y-8">
                 <div className="flex flex-col items-center gap-8">
-                    logo
+                    <Image src="https://cdn.vectorstock.com/i/1000x1000/51/71/initial-monogram-letter-nc-logo-design-template-vector-29675171.webp" alt="logo" width={50} height={50} className='rounded-full' />
                     <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
                         Sign in to your account
                     </h2>
